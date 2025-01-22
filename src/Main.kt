5ad8a681385a38b3ee94a4b34e3c490f030435aa
juanmorales879 <juanmorales.marcano@gmail.com>
2025-01-21 20:43:27 -0800

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,43 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    //if - else (5)
+
+    //when (5)
+
+    //for (5)
+
+    //while (5)
+
+    // fib
+
+    val n = 4
+    var prev = 0
+    var next = 1
+    var fib = 0
+
+    for (i in 2.. n) { // Start from 0
+        fib = prev + next
+        prev = next
+        next = fib
+    }
+
+    println(fib) // Output will still be 3 for n = 4
+
+    var limit = 2
+    var previous = 0
+    var following = 1
+    var fibonacci = 0
+
+    while (limit <= 4){
+        fibonacci = previous + following
+        previous = following
+        following = fibonacci
+        limit++
+    }
+    println(fibonacci)
+
+
+
+}
